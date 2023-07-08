e355b3bd94d2ba6b37f78d8926ec8d919cb04ae8
c.misman <mismancelaleddin@gmail.com>
2023-07-08 08:47:24 +0300

    gateway routes added for soda services

---
+++ src/main/java/com/ms/gateaway/config/LocalHostRouteConfig.java
@@ -0,0 +1,21 @@
+package com.ms.gateaway.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class LocalHostRouteConfig {
+
+    @Bean
+    public RouteLocator localeHostRoutes(RouteLocatorBuilder builder){
+        return builder.routes()
+                .route(r -> r.path("/api/v1/soda*","/api/v1/soda/*", "/api/v1/soda/upc/*")
+                        .uri("http://localhost:8080"))
+                .route(r -> r.path("/api/v1/customers/**")
+                        .uri("http://localhost:8081"))
+                .build();
+    }
+
+}
